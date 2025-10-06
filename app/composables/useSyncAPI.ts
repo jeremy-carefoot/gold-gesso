4fe1fb388a82bcb7b268f59cd0a61a18d1ff21e9
Jeremy Carefoot <jeremycarefoot@gmail.com>
2025-10-06 12:28:43 -0600

    Add trailing backslashes to sync api requests to avoid 301 errors

--- app/composables/useSyncAPI.ts
+++ app/composables/useSyncAPI.ts
@@ -11,11 +11,11 @@ export const useSyncAPI = <T>(
     const { $api } = useNuxtApp();
 
     // Data from DB
-    const cachedFetch = useAPI<T>(`${url}/cached`, options);
+    const cachedFetch = useAPI<T>(`${url}/cached/`, options);
 
     // Request to sync with external API and refresh cached data (client side only)
     onMounted(() => {
-        $api(`${url}/update`, {
+        $api(`${url}/update/`, {
             onResponse({ response }) {
                 if (response.status !== 200) {
                     console.error(`Could not sync cached data: ${url}`);
--- app/plugins/api.ts
+++ app/plugins/api.ts
@@ -3,7 +3,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     const { token: authToken } = useAuth();
 
     const api = $fetch.create({
-        async onRequest({ options }) {
+        async onRequest({ request, options }) { 
             const authHeaders: Record<string, string> = {};
 
             if (authToken.value) {
@@ -15,7 +15,7 @@ export default defineNuxtPlugin((nuxtApp) => {
             options.headers = {
                 ...options.headers,
                 ...authHeaders
-            }
+            };
         },
         onResponse({ response }) {
             // Add any custom on response events here (could be debugging)
